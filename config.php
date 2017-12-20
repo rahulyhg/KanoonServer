3111a955abf3663a2d10458fb9dc94f148e8e05d
Ravikant <ravikant.vishwakarma@hotmail.com>
2017-12-20 17:51:22 +0530

    Initial Commit
    Signed-off-by: Ravikant <ravikant.vishwakarma@hotmail.com>

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
  * Time: 05:37 PM
  */
 
-class config
-{
-
-}
+define('HOST','mysql.hostinger.in');
+define('USER','u480707125_kanon');
+define('PASS','*/9004137765');
+define('DB','u480707125_kanon');
